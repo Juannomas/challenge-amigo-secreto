74b100040e7c376271570f18cbd5940030287bc7
Juannomas <juan.m.inzulza@gmail.com>
2025-02-08 03:20:36 -0300

    termino código app.js

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let arrayNombres = [];
+let arrayNombresSorteados = [];
 
 function agregarEnArrayNombres() {
     let nombre = (document.getElementById('amigo').value);
     if (nombre == '') {
         alert('Por favor, inserte un nombre.');
     } else {
-        arrayNombres.push(nombre);
-        document.querySelector('#amigo').value = '';
-        console.log (arrayNombres);
-        let li = document.createElement("li");
-        li.innerHTML = '';
-            for (let i = 0; i < arrayNombres.length; i++){
-            li.innerText = arrayNombres[i];
-            let ul1 = document.getElementById ('listaAmigos');
-            ul1.appendChild(li);
-            console.log (listaAmigos);    
+        if (arrayNombres.includes(nombre)){
+            alert('Lo siento, ya existe ese nombre');
+            document.querySelector('#amigo').value = '';    
+        } else {
+            arrayNombres.push(nombre);
+            document.querySelector('#amigo').value = '';
+            let li = document.createElement("li");
+            li.innerHTML = '';
+                for (let i = 0; i < arrayNombres.length; i++){
+                li.innerText = arrayNombres[i];
+                let ul1 = document.getElementById('listaAmigos');
+                ul1.appendChild(li);   
+                }
+        }   
+    }
+}
+
+
+function sortearAmigo() {
+    if (arrayNombres.length <= 2 ) {
+        alert('Por favor, inserte a lo menos 3 nombres.');
+    } else {
+        let i1 =  parseInt(Math.floor(Math.random()*arrayNombres.length));
+        console.log (i1);
+        if (arrayNombresSorteados.includes(arrayNombres[i1])){
+            return sortearAmigo();
+        } else {
+            arrayNombresSorteados.push(arrayNombres[i1]);
+            console.log(arrayNombresSorteados);
+            if (arrayNombres.length == arrayNombresSorteados.length) {
+            alert('Ya se sortearon todos los Nombres.');  
+            } else {
+            let ul2 = document.getElementById('resultado');
+            ul2.innerHTML = arrayNombres[i1];
             }
+        }    
     }
 }
