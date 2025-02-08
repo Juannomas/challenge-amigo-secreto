62751cb27cb09e20b2c124cc2e8a6b4afb8c9317
Juannomas <juan.m.inzulza@gmail.com>
2025-02-08 00:29:57 -0300

    agrega funcion en app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaAmigos = [];
+let arrayNombres = [];
 
-
-function agregarAmigo() {
+function agregarEnArrayNombres() {
     let nombre = (document.getElementById('amigo').value);
     if (nombre == '') {
-        alert('mal');
+        alert('Por favor, inserte un nombre.');
     } else {
-    listaAmigos.push(nombre);
-    console.log (listaAmigos);
-    document.querySelector('#amigo').value = '';
+        arrayNombres.push(nombre);
+        document.querySelector('#amigo').value = '';
+        console.log (arrayNombres);
+        let li = document.createElement("li");
+        li.innerHTML = '';
+            for (let i = 0; i < arrayNombres.length; i++){
+            li.innerText = arrayNombres[i];
+            let ul1 = document.getElementById ('listaAmigos');
+            ul1.appendChild(li);
+            console.log (listaAmigos);    
+            }
     }
 }
